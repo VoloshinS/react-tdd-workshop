39baf9bf8989def94dcc7f27a43256fb08c5dfa4
sergey.voloshin <sergey.voloshin@nextiva.com>
2018-03-10 15:33:38 +0200

    Complete game winning logic

--- src/gameService.js
+++ src/gameService.js
@@ -1,9 +1,31 @@
+const transpose = matrix => matrix[0].map((_, cell) => matrix.map(row => row[cell]));
+const isWinRow = (board, symbol) => board.some(row => row.every(cell => cell === symbol));
+const isWinLine = (board, symbol) => isWinRow(transpose(board), symbol);
+const isWinDiagonal = (board, symbol) => {
+  // @TODO: Find better solution;
+  const transposeDiagonales = [
+    [board[0][0], board[1][1], board[2][2]],
+    [board[0][2], board[1][1], board[2][0]],
+  ];
+
+  return isWinRow(transposeDiagonales, symbol);
+};
+
+const isWin = (board, symbol) =>
+  isWinRow(board, symbol) || isWinLine(board, symbol) || isWinDiagonal(board, symbol);
+
+const isTie = board => !board.some(row => row.some(cell => cell === ''));
+
 export const gameStatus = board => {
-  const isWin = symbol => board[0].every(cell => cell === symbol);
-  if (isWin('X')) {
+  if (isWin(board, 'X')) {
     return 'X';
   }
-  if (isWin('O')) {
+
+  if (isWin(board, 'O')) {
     return 'O';
+  }
+
+  if (isTie(board)) {
+    return 'tie';
   }
 };
